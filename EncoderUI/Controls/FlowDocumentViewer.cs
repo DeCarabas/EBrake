f464176c2fdd82253ef046eb8b39d7b54a533f02
John Doty <jddoty@gmail.com>
2011-12-30 01:05:31 -0800

    Some additional prettiness in the about window, and fix mouse-wheel scrolling.

---
+++ EncoderUI/Controls/FlowDocumentViewer.cs
@@ -0,0 +1,13 @@
+﻿namespace EncoderUI.Controls
+{
+    using System.Windows.Controls;
+    using System.Windows.Input;
+
+    public class FixedFlowDocumentViewer : FlowDocumentScrollViewer
+    {
+        protected override void OnMouseWheel(MouseWheelEventArgs e) 
+        { 
+            // Ignore 
+        }
+    }
+}
