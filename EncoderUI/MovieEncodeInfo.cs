93da6d0c8f25faaae0fd6e26c646e06245f52d36
John Doty <jddoty@gmail.com>
2012-01-07 06:59:00 -0800

    Only change backdrop on exact match, make the search interval smaller

--- EncoderUI/MovieEncodeInfo.cs
+++ EncoderUI/MovieEncodeInfo.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Collections.ObjectModel;
     using System.IO;
     using System.Linq;
     using System.Threading;
@@ -19,13 +20,14 @@
         string backdrop = stockBackdrop;
         string movieTitle;
         string movieYear;
+        readonly ObservableCollection<TmdbMovie> potentialMatches = new ObservableCollection<TmdbMovie>();
         CancellationTokenSource queryCancellationSource;
         DispatcherTimer queryTimer = new DispatcherTimer();
 
         public MovieEncodeInfo(MainWindow mainWindow, Queue encodingQueue)
             : base(mainWindow, encodingQueue)
         {
-            this.queryTimer.Interval = TimeSpan.FromSeconds(1);
+            this.queryTimer.Interval = TimeSpan.FromMilliseconds(500);
             this.queryTimer.Tick += QueryMetadata;
             this.queryTimer.IsEnabled = false;
         }
@@ -38,19 +40,19 @@
 
         public override InfoError InfoError
         {
-            get 
+            get
             {
                 if (String.IsNullOrWhiteSpace(MovieTitle)) { return InfoError.NoMovieTitle; }
                 if (SourceDrive != null && !SourceDrive.IsReady) { return InfoError.DiscNotReady; }
-                return InfoError.OK; 
+                return InfoError.OK;
             }
         }
 
         public string MovieTitle
         {
             get { return this.movieTitle; }
-            set 
-            { 
+            set
+            {
                 this.movieTitle = value;
 
                 if (this.queryTimer.IsEnabled) { this.queryTimer.Stop(); }
@@ -66,6 +68,8 @@
             set { this.movieYear = value; Notify("MovieYear"); }
         }
 
+        public IList<TmdbMovie> PotentialMatches { get { return this.potentialMatches; } }
+
         protected override void AddEncodingJobs(List<Job> encodingQueue)
         {
             string baseName = MovieTitle.Trim();
@@ -73,18 +77,18 @@
 
             string movieDirectory = Path.Combine(OutputPath, EscapeFileName(baseName));
             if (!Directory.Exists(movieDirectory)) { Directory.CreateDirectory(movieDirectory); }
-            
+
             string outputFile = Path.Combine(movieDirectory, EscapeFileName(baseName) + ".m4v");
 
-            string commandLine = "--main-feature " + 
+            string commandLine = "--main-feature " +
                 GetStandardCommandLine(Path.Combine(SourceDrive.RootDirectory.FullName, "VIDEO_TS"), outputFile);
 
             encodingQueue.Add(new Job
             {
-                Query = commandLine, 
-                Title = 0, 
-                Source = SourceDrive.RootDirectory.FullName, 
-                Destination = outputFile, 
+                Query = commandLine,
+                Title = 0,
+                Source = SourceDrive.RootDirectory.FullName,
+                Destination = outputFile,
                 CustomQuery = true
             });
         }
@@ -122,15 +126,20 @@
             if (task.Status == TaskStatus.Canceled || token.IsCancellationRequested) { return; }
 
             string newBackdrop = null;
+            TmdbMovie[] movies = null;
             try
             {
-                TmdbMovie[] movies = task.Result;
-                if (movies.Length > 0 && movies[0].Backdrops.Count > 0)
+                movies = task.Result;
+                if (movies.Length > 0)
                 {
                     TmdbMovie movie = movies[0];
-                    TmdbImage image = movie.Backdrops.OrderByDescending(i => i.Image.Width).First();
-                    newBackdrop = image.Image.Url;
+                    if (String.Equals(movie.Name, MovieTitle, StringComparison.CurrentCultureIgnoreCase))
+                    {
+                        TmdbImage image = movie.Backdrops.OrderByDescending(i => i.Image.Width).First();
+                        newBackdrop = image.Image.Url;
+                    }
                 }
+
             }
             catch
             {
@@ -138,6 +147,11 @@
             }
 
             Backdrop = newBackdrop ?? stockBackdrop;
+            this.potentialMatches.Clear();
+            if (movies != null)
+            {
+                for (int i = 0; i < movies.Length; i++) { this.potentialMatches.Add(movies[i]); }
+            }
         }
     }
 }
